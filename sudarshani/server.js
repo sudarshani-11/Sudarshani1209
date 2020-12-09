4bbe0a45793b84f035b890033881ac35c93ed379
sudarshani-11 <74652767+sudarshani-11@users.noreply.github.com>
2020-12-09 22:05:22 +0530

    Update server.js

--- sudarshani/server.js
+++ sudarshani/server.js
@@ -1,29 +1,29 @@
-
-var express = require('express');
-var app = express();
-var path=require("path");
-var fs=require("fs");
-
-
-//server configuration
-app.use(express.static(path.join(__dirname,'Sk')));
-
- app.get('/', function (req, res) {
-   res.sendFile(path.join(___dirname + '/index.html'));
+const http = require("http");
+const url = require("url");
+const querystring = require("querystring");
+const fs = require("fs");
+var processRequest = (req,resp) => {
+const p = url.parse(req.url);
+switch(p.pathname)
+{
+  case "/":
+fs.readFile("index.html",(err,data)=>{
+if(err)
+{
+console.log("failed to load");
+}
+else{
+resp.end(data);
+}
 });
- 
+break;
+case "/validate":
+const q = querystring.parse(p.query);
+resp.end(q.name+" login successfully");
 
-app.get('/hello', function (req, res) {
-  console.log("CAlling rest api");
-  var person={firstName:'Ravi',lastName:'Tambade',age:43};
-  res.send(person);
-});
-
- 
-var server = app.listen(8081, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("Example app listening at http://localhost:8081", host, port)
-})
+break;
+}
+}
+const server = http.createServer(processRequest);
+server.listen(8081);
+console.log("server started");
